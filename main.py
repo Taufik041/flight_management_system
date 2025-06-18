5aaf0fbb0fdcd2367ff1c5b5fff41c7d7d92b22f
Taufik <taufik4051@gmail.com>
2025-06-18 16:57:58 +0530

    added favicon

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
 from logger import logger
+import os
 
 
 app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+@app.get("/favicon.ico")
+async def favicon():
+    return FileResponse(os.path.join("static", "favicon.ico"))
+
 
 @app.get("/")
 def read_root():
     logger.info("Root endpoint accessed")
     return {"message": "Hello World"}
+
